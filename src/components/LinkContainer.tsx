991821f27b8b44193aac10d3925e42c104eee587
Gabriel Ramires <falivith@gmail.com>
2023-06-11 20:43:23 -0300

    First Commit

---
+++ src/components/LinkContainer.tsx
@@ -0,0 +1,28 @@
+import styles from './LinkContainer.module.css'
+
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
+
+export function LinkContainer () {
+
+    const size = 5;
+
+    return (
+        <div>
+            <a href = "https://www.linkedin.com/in/gabriel-ramires-42a8a61a0/" target = "_blank">
+                <AiFillLinkedin style = {{ fontSize: size + 'rem' }} />
+            </a>
+            <a href = "https://github.com/Falivith" target = "_blank">
+                <AiFillGithub style = {{ fontSize: size + 'rem' }} />
+            </a>
+            <a href = "https://www.facebook.com/Falivith/" target = "_blank">
+                <AiFillFacebook style = {{ fontSize: size + 'rem' }} />
+            </a>
+            <a href = "https://www.instagram.com/gabriel_falivith/" target = "_blank">
+                <AiFillInstagram style = {{ fontSize: size + 'rem' }} />
+            </a>
+        </div>
+    )
+}
