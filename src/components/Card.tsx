52ceaec1b21dfcb88e394f05faba17c913a56c48
Gabriel Ramires <falivith@gmail.com>
2023-06-13 02:14:52 -0300

    Card Redesign

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,17 @@ import styles from "./Card.module.css";
 export function Card (props: CardProps) {
     return (
         <div className = {styles.cardContainer}>
-            <div>
+            <div className = {styles.internalHeader}>
                 <img className = {styles.companyPic } src = {props.companyPic} alt = "Company Picture"/>
-                <h2 className = {styles.title }> {props.title}</h2>   
+                <div className = {styles.titleContainer }>
+                    <h2 className = {styles.title }> {props.title} </h2>   
+                    <h3 className = {styles.title2 }> {props.title2} </h3>   
+                </div>
             </div>
-            <span>{props.date}</span>
-            <p>{props.description}</p>
+            <div className = {styles.textSection}>
+                <p className = {styles.description}>{props.description}</p>
+                <span className = {styles.date}>{props.date}</span>    
+            </div>
         </div>
     )
 }
@@ -16,6 +21,7 @@ export function Card (props: CardProps) {
 interface CardProps {
     companyPic: string;
     title: string;
+    title2: string;
     date: string;
     description: string;
 }
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,23 +1,44 @@
 import styles from "./Experience.module.css";
 import { Card } from "./Card";
 import Hut8 from "../assets/HUT8.svg";
+import Ufpel from "../assets/UFPEL.png";
+import Fapergs from "../assets/Fapergs.png";
 
 export function Experience () {
 
     const experiences = [
         {
             companyPic: Hut8,
-            title: "Monitor Voluntário (Lógica para Computação)",
+            title: "Monitor Voluntário",
+            title2:  "(Lógica para Computação)",
             date: "01/2022 à 06/2022",
             description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
         },
         {
-            title: "Monitor (Programação de Computadores)",
+            companyPic: Ufpel,
+            title: "Monitor",
+            title2:  "(Programação de Computadores)",
             date: "01/2022 à 06/2022",
             description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
         },
         {
-            title: "Desenvolvedor Web FullStack (Pesquisa)",
+            companyPic: Fapergs,
+            title: "Desenvolvedor Web FullStack",
+            title2:  "(Bolsista de Pesquisa)",
+            date: "01/2022 à 06/2022",
+            description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
+        },
+        {
+            companyPic: Ufpel,
+            title: "Monitor",
+            title2:  "(Programação de Computadores)",
+            date: "01/2022 à 06/2022",
+            description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
+        },
+        {
+            companyPic: Ufpel,
+            title: "Monitor",
+            title2:  "(Programação de Computadores)",
             date: "01/2022 à 06/2022",
             description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
         }
@@ -30,8 +51,9 @@ export function Experience () {
                 {experiences.map((experience, index) => {
                     return <Card
                         key = {index}
-                        companyPic = {Hut8}
+                        companyPic = {experience.companyPic}
                         title = {experience.title}
+                        title2 = {experience.title2}
                         date = {experience.date}
                         description = {experience.description}
                 />})}
