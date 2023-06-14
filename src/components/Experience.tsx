837438e4cfa3e794590a2c2ed9027962fff5827b
Gabriel Ramires <falivith@gmail.com>
2023-06-14 19:29:21 -0300

    Card Changes

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -23,24 +23,24 @@ export function Experience () {
         },
         {
             companyPic: Fapergs,
-            title: "Desenvolvedor Web FullStack",
-            title2:  "(Bolsista de Pesquisa)",
+            title: "Desenvolvedor Web FullStack (Bolsista)",
+            title2:  "Projeto de Ensino e Extensão",
             date: "01/2022 à 06/2022",
-            description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
+            description: "Desenvolvimento de uma extensão de navegador responsável por coletar, processar e armazenar dados, utilizando HTML, CSS e JavaScript. A extensão trabalha com requisições para APIs externas e web scraping. Além disso, a construção da interface da aplicação é feita utilizando React, JS, CSS com módulos, SCSS e outras tecnologias, integrando-a com a API desenvolvida. A API é construída utilizando Node, Express, Sequelize, Multer, Insomnia, REST e outras tecnologias, disponibilizando serviços úteis para consulta dos dados da aplicação. Também é realizado o modelagem do banco de dados relacional da aplicação, implementado com PostgreSQL e integrado com a API desenvolvida. O trabalho envolve o uso do banco de dados PostgreSQL, além de sistemas de registro e login de usuários com oAuth 2.0. Todo o desenvolvimento é realizado em equipe com mais 3 integrantes, incluindo designers e desenvolvedores, utilizando Trello para a organização de tarefas."
         },
         {
             companyPic: Ufpel,
-            title: "Monitor",
-            title2:  "(Programação de Computadores)",
-            date: "01/2022 à 06/2022",
-            description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
+            title: "Monitor (Bolsista)",
+            title2:  "Programação de Computadores",
+            date: "06/2022 à 12/2022",
+            description: "Monitoria na Disciplina de Programação de Computadores. Envolve o ensino de programação de computadores, com foco na linguagem C e suas estruturas básicas. Além disso, inclui a produção de material didático relacionado ao curso, a resposta às questões dos alunos e a revisão dos códigos dos alunos, ajudando-os a resolver bugs."
         },
         {
             companyPic: Ufpel,
-            title: "Monitor",
-            title2:  "(Programação de Computadores)",
+            title: "Monitor Voluntário",
+            title2:  "Lógica de Programação",
             date: "01/2022 à 06/2022",
-            description: "As a mentor to students on the Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error harum, quos excepturi officia velit eaque. Repellendus harum et nemo animi. Repellat, ducimus? Voluptatibus eligendi similique ullam quaerat ducimus excepturi!"
+            description: "Funções: Ensinar conceitos de lógica para alunos, como lógica booleana, tabelas verdade, árvores de refutação, etc. Produzir material didático relacionado à disciplina, responder às questões dos alunos e revisar exercícios dos alunos, corrigindo-os e ajudando-os na resolução."
         }
     ]
 
